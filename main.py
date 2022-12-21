e34e12bebe432ac329e089df02b9d33057737bb3
atms <etiennedtboivin@gmail.com>
2022-12-21 11:35:52 -0800

    pull_data function initialized along with user_input functionality

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from pull_data import pull_data
+from user_input import user_input
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +8,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pull_data()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
