bdd952767b65c071d4b6b85dd525ca73d0de55ef
atms <etiennedtboivin@gmail.com>
2022-12-21 12:28:54 -0800

    user input functionality added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pull_data()
+    years = user_input()
+    pull_data(years)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
