e76e065ef58234d212699db443971e0b7bef5626
atms <etiennedtboivin@gmail.com>
2022-12-25 05:40:57 -0800

    Ball position visualizer with football field draw added. On hold until pipeline is fleshed out.

--- pull_data.py
+++ pull_data.py
@@ -2,6 +2,7 @@ import nfl_data_py as nfl
 import pandas as pd
 import numpy as np
 
-
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.max_rows', 1000)
 def pull_data(years, columns=None):
     return nfl.import_pbp_data(years=years, columns=columns)
