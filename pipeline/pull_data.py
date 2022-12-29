43e9fb562f4331b795d728f9f97917c34062bc37
atms <etiennedtboivin@gmail.com>
2022-12-29 09:07:54 -0800

    Fixed a few errors

--- pipeline/pull_data.py
+++ pipeline/pull_data.py
@@ -6,11 +6,6 @@ pd.set_option('display.max_columns', 1000)
 pd.set_option('display.max_rows', 1000)
 
 
-<<<<<<< HEAD
-def pull_data(years, columns=None):
-    return nfl.import_pbp_data(years=years, columns=columns)
-
-
 def pull_wp_data(years):
     df = nfl.import_pbp_data(years=years)
     df.loc[:, 'time_elapsed'] = 3600 - df['game_seconds_remaining'].copy()
@@ -19,14 +14,5 @@ def pull_wp_data(years):
         if not df.loc[game_mask, 'game_half'].isin(['Overtime']).empty:
             ot_mask = (df['game_half'] == 'Overtime')
             df.loc[ot_mask, 'time_elapsed'] = 4500 - df['game_seconds_remaining'].copy()
-    return df
-def pull_wp_data(years, columns=None):
-    df = nfl.import_pbp_data(years=years, columns=columns)
-    df["time_elapsed"] = 3600 - df['game_seconds_remaining']
-    for game in df.game_id.unique():
-        mask = df['game_id'] == game
-        if not (df.loc[mask, 'game_half'] == 'Overtime').empty:
-            ot_mask = df['game_half'] == 'Overtime'
-            df.loc[ot_mask, "time_elapsed"] = 4500 - df['game_seconds_remaining']
     return df
 
