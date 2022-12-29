34255d69fd5064c025ab59bc97fdf7a1e25e0174
atms <etiennedtboivin@gmail.com>
2022-12-28 18:17:47 -0800

    Added win probability visualization functionality to dashboard

--- pipeline/pull_data.py
+++ pipeline/pull_data.py
@@ -4,5 +4,15 @@ import numpy as np
 
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.max_rows', 1000)
-def pull_data(years, columns=None):
-    return nfl.import_pbp_data(years=years, columns=columns)
+
+
+def pull_wp_data(years, columns=None):
+    df = nfl.import_pbp_data(years=years, columns=columns)
+    df["time_elapsed"] = 3600 - df['game_seconds_remaining']
+    for game in df.game_id.unique():
+        mask = df['game_id'] == game
+        if not (df.loc[mask, 'game_half'] == 'Overtime').empty:
+            ot_mask = df['game_half'] == 'Overtime'
+            df.loc[ot_mask, "time_elapsed"] = 4500 - df['game_seconds_remaining']
+    return df
+
